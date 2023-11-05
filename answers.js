559e3000210a9d8af5793dc372eb041cbb7473ef
Timothy Rodriguez <tim.rdrgz2017@live.com>
2023-11-05 13:39:17 -0600

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -30,8 +30,23 @@ for (let i = 1; i <=100; i++) {
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+plantee [2] = 5001;
+console.log(plantee)
 
+wolfy [3] = "Gotham City";
+console.log(wolfy)
+
+dart[3] = "Upside Down & Hawkins";
+console.log(dart)
+
+wolfy[0] = "Gameboy";
+console.log(wolfy)
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
