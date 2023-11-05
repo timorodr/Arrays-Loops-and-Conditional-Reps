31522b4635ffa3221595f4396231b577e3f8e9ef
Timothy Rodriguez <tim.rdrgz2017@live.com>
2023-11-05 13:22:11 -0600

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -15,8 +15,18 @@ for (let i = 0; i <= 200; i+=2) {
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
-
+//Write a javascript application that logs all numbers from 1 - 100.
+for (let i = 1; i <=100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log ("Fizz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
